f5631c8e6e4f06228a4e49090f10f1b90f7931f0
Dichik <omeluan.dima@gmail.com>
2023-12-25 12:16:29 +0200

    baseline for client

---
+++ client/src/sellers/SellerService.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:80/sellers";
+
+export const createSeller = (data: any) => axios.post(`${API_URL}`, data);
+export const getAllSellers = () => axios.get(`${API_URL}`);
+export const getSeller = (id: any) => axios.get(`${API_URL}/${id}`);
+export const updateSeller = (id: any, data: any) =>
+  axios.patch(`${API_URL}/${id}`, data);
+export const deleteSeller = (id: any) => axios.delete(`${API_URL}/${id}`);
---
+++ client/src/viewer/ViewerService.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:80/viewers";
+
+export const createViewer = (data: any) => axios.post(`${API_URL}`, data);
+export const getAllViewers = () => axios.get(`${API_URL}`);
+export const getViewer = (id: any) => axios.get(`${API_URL}/${id}`);
+export const updateViewer = (id: any, data: any) =>
+  axios.patch(`${API_URL}/${id}`, data);
+export const deleteViewer = (id: any) => axios.delete(`${API_URL}/${id}`);
